722cd4c33e8976f693941f39797a8da25e327996
djsoul86 <ernestoandrade86@gmail.com>
2018-04-12 19:56:54 -0500

    Agregando Data Table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {routes} from './routes';
 import { RouterModule } from '@angular/router'; 
@@ -7,11 +8,29 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { MantenimientoComponent } from './auth/mantenimiento/mantenimiento.component';
 import { HomeComponent } from './auth/home/home.component';
-import { MAT_CHECKBOX_CLICK_ACTION }  from '@angular/material';
+import {ConsultarTipopagoService} from './auth/consultar-tipopago/services/consultar-tipopago.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//Models
+import { TipoPagoResponse } from './auth/consultar-tipopago/models/tipopago-response.model';
+
+
 
 //Material
 import { CrearTipopagoComponent } from './auth/crear-tipopago/crear-tipopago.component';
-import { MatInputModule,MatListModule,MatIconModule,MatCheckboxModule,MatButtonModule } from '@angular/material';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MAT_CHECKBOX_CLICK_ACTION
+  ,MatButtonModule
+  ,MatInputModule
+  ,MatTableModule
+  ,MatMenuModule
+  ,MatPaginatorModule
+  ,MatSortModule
+  ,MatIconModule
+  ,MatDialogModule
+  ,MatPaginatorIntl
+  ,MatListModule
+  ,MatCheckboxModule } from '@angular/material';
 import { ConsultarTipopagoComponent } from './auth/consultar-tipopago/consultar-tipopago.component';
 
 @NgModule({
@@ -22,20 +41,28 @@ import { ConsultarTipopagoComponent } from './auth/consultar-tipopago/consultar-
     HomeComponent,
     CrearTipopagoComponent,
     ConsultarTipopagoComponent
+    
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule,
     RouterModule.forRoot(routes),
+    HttpClientModule,
     //Material
-    MatListModule,
-    MatIconModule,
     MatInputModule,
-    MatCheckboxModule,
-    MatButtonModule
+    MatSortModule,
+    MatMenuModule,
+    MatIconModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatListModule,
+    MatCheckboxModule
   ],
   providers: [
+    ConsultarTipopagoService,
     {provide: MAT_CHECKBOX_CLICK_ACTION, useValue: 'check'}
   ],
   bootstrap: [AppComponent]
--- src/app/auth/consultar-tipopago/consultar-tipopago.component.ts
+++ src/app/auth/consultar-tipopago/consultar-tipopago.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { ConsultarTipopagoService } from './services/consultar-tipopago.service';
+import {TipoPagoResponse} from '../consultar-tipopago/models/tipopago-response.model';
+import { TipoPago } from './models/tipopago-model';
+import { MatTableDataSource,MatPaginator,MatSort, MatDialog, PageEvent } from '@angular/material';
 
 @Component({
   selector: 'app-consultar-tipopago',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./consultar-tipopago.component.css']
 })
 export class ConsultarTipopagoComponent implements OnInit {
+  tipopago:Array<TipoPago>;
+  displayedColumns = ['id','nombrepago','fijo','valor','promedio'];
+  dataSource;
+  @ViewChild(MatPaginator) paginator:MatPaginator;
+  @ViewChild(MatSort) sort:MatSort;
 
-  constructor() { }
+  length = 50;
+  pageSize = 5;
+  pageSizeOptions = [5, 10, 20];
+  pageEvent:PageEvent;
+  
+  applyFilter(filterValue:string){
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLocaleLowerCase();
+    this.dataSource.filter = filterValue;
+  }
+
+  setPageSizeOptions(setPageSizeOptionsInput:string){
+    this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+  }
+  constructor(public consultartipopagoservice:ConsultarTipopagoService) { }
 
   ngOnInit() {
+    this.consultartipopagoservice.getAll().subscribe(
+      (data:any) =>
+    {
+      this.tipopago = data;
+      this.dataSource = new MatTableDataSource<TipoPago>(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      console.log(data);
+      console.log(data.tipopago);
+      console.log(this.tipopago);
+    },
+    error=>{
+      console.error(error);
+    }
+  );
   }
 
 }
